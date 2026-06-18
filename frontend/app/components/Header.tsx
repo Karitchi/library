ab42c49fc0c54b619aaa0c4a888fdf11e85182d7
Karitchi <karichikarichikarichi@gmail.com>
2026-06-18 16:53:03 +0200

    Revert "hide avatar menu when not logged in"
    This reverts commit 116e504f04d333b0287c9d0a43870ce5227e399c.

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -2,14 +2,12 @@ import { Link } from "react-router";
 import { AvatarMenu } from "./AvatarMenu";
 
 export function Header() {
-  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
-
   return (
     <div className="flex items-center my-5">
       <div className="flex-1" />
       <Link to="/books" className="text-center text-6xl font-jacquard hover:underline">Librarian</Link>
       <div className="flex-1 flex justify-end">
-        {token && <AvatarMenu />}
+        <AvatarMenu />
       </div>
     </div>
   );
